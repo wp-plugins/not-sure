66544c43ab2ee1febea55083887015147ec5e4ac
softpill.eu <softpill.eu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-10 12:43:55 +0000

    Edited about page
    git-svn-id: https://plugins.svn.wordpress.org/not-sure/trunk@1178209 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/notsure_about.php
+++ includes/notsure_about.php
@@ -17,6 +17,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 function sp_ns_about(){
 ?>
+<h2>About Not Sure</h2>
+<p>Thank you for installing Not Sure on your Wordpress website, we hope you like our plugin.</p>
+<p>Don't forget to <a href="http://www.softpill.eu/not-sure-plugin" target="_blank">rate the plugin</a>.</p>
+<p>Visit <a href="http://www.softpill.eu/" target="_blank">softpill.eu</a> for more great extensions.</p>
+
 <h2>How Not Sure works</h2>
 <p>
 Place Not Sure buttons on the pages you want to ask customers why they are unsure of purchasing your products/services.
@@ -24,9 +29,11 @@ Place Not Sure buttons on the pages you want to ask customers why they are unsur
 <p>
 Create Not sure buttons and place the shortcode in your posts.
 </p>
-<p>
-Enjoy!
-</p>
+<h2>If you require support</h2>
+<p>Buy the plugin here: <a href="http://www.softpill.eu/not-sure-plugin" target="_blank">www.softpill.eu/not-sure-plugin</a>. Request support: <a href="http://www.softpill.eu/support" target="_blank">www.softpill.eu/support</a></p>
+<h2>If you require custom work done</h2>
+<p>Don't hesitate to contact us <a href="http://www.softpill.eu/contact-us" target="_blank">www.softpill.eu/contact-us</a>, take a look on <a href="http://www.softpill.eu/payment" target="_blank">our rates</a>.</p>
+<h2>Enjoy!</h2>
 <?php
 }
 ?>
